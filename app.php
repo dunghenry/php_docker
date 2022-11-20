45051f416935ebabd5318fb14dc75a80184f66c3
dunghenry <trandungksnb6930082001>
2022-11-20 23:25:31 +0700

    add function and strict mode

--- app.php
+++ app.php
@@ -1,5 +1,11 @@
-<?php
+<?php declare(strict_types=1);
     $a = 10;
     echo '<h1>Hello, World!</h1>';
+    echo '<br>';
     echo $a;
+    function sum(int $a, int $b) {
+        return $a+ $b;
+    };
+    echo '<br>';
+    echo sum(1, 2);
 ?>
